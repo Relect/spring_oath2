1fbcd8b95ed349f2b6e5a623399eed786f7fc802
Gennadiy <relect@bk.ru>
2024-11-08 14:21:04 +0300

    first commit

---
+++ src/main/java/com/example/srpint_oath2/SrpintOath2Application.java
@@ -0,0 +1,13 @@
+package com.example.srpint_oath2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrpintOath2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrpintOath2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/srpint_oath2/SrpintOath2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.srpint_oath2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrpintOath2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
